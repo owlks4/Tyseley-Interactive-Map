cdddd2b131829cd71152c874341c87708efb880d
owlks4 <owilkes250@gmail.com>
2023-12-04 23:30:05 +0000

    Add tyseley materials

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 import './style.css'
 import 'leaflet/dist/leaflet.css';
 import L from "leaflet";
+import bg from "/big_tyseley.png"
 
-var map_init = L.map('map',{
+var map = L.map('map',{
   center: [0, 0],
-  zoom:1
+  zoom:1,
+  minZoom: 1,
+  maxZoom: 4
 });
 
-
-var imageUrl = 'https://maps.lib.utexas.edu/maps/historical/newark_nj_1922.jpg',
-    imageBounds = [[0, 0], [40.773941, 74.12544]];
-L.imageOverlay(imageUrl, imageBounds).addTo(map_init);
+var imageUrl = bg,
+    imageBounds = [[-251.092651757, -400], [251.092651757, 400]];
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 var greenIcon = L.icon({
   iconUrl: 'filename',
   });
 
-L.marker([9.0820, 8.6753],  {icon: greenIcon}).addTo(map_init)
+L.marker([9.0820, 8.6753],  {title: "TEXT", riseOnHover: true, autoPanOnFocus: true}).addTo(map)
